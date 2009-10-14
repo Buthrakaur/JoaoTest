95b36551d708547e1ffc6cb2db7b57e6e974b0cb
unknown <Filip@.(none)>
2009-10-14 10:16:39 +0200

    init commit

---
+++ Test/Test/FindTopValuesTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Test
+{
+	[TestFixture]
+	public class FindTopValuesTests
+	{
+		[Test(Description = "FindMaxValue method should throw ArgumentException when empty array supplied as a parameter."),
+		ExpectedException(typeof(ArgumentException))]
+		public void FindMaxValue_Should_ThrowWhenEmptyArrayPassed()
+		{
+			var values = new int[0];
+			var finder = new FindTopValues();
+			finder.FindMaxValue();
+		}
+	}
+}
---
+++ Test/Test/IFindTopValues.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test
+{
+	public interface IFindTopValues
+	{
+		int FindMaxValue(int[] anyOldOrderValues);
+		int[] FindTopNValues(int[] anyOldOrderValues, int n);
+	}
+}
